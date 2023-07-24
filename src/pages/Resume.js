7849343495a4f4ff849fc625d76ba7cb8974d079
TRex-007-15 <100136615+TRex-007-15@users.noreply.github.com>
2023-07-24 22:13:57 +0530

    Update Resume.js

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,63 @@
-import React, { useState } from 'react';
-import { Button, Space, Table } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Button, Space, Table, Input, Select } from 'antd';
 
 const Resume = () => {
   const [filteredInfo, setFilteredInfo] = useState({});
   const [sortedInfo, setSortedInfo] = useState({});
+  const [data, setData] = useState([]);
+
+  // Load data from local storage on initial render
+  useEffect(() => {
+    const savedData = localStorage.getItem('resumeData');
+    if (savedData) {
+      setData(JSON.parse(savedData));
+    } else {
+      // If no data found in local storage, initialize with sample data
+      setData([
+        {
+          key: '1',
+          name: 'John Brown',
+          age: 32,
+          email: 'john@example.com',
+          gender: 'Male',
+          address: '123 Main St',
+          designation: 'Captain',
+        },
+        {
+          key: '2',
+          name: 'Jim Green',
+          age: 42,
+          email: 'jim@example.com',
+          gender: 'Male',
+          address: '456 Park Ave',
+          designation: 'First Mate',
+        },
+        {
+          key: '3',
+          name: 'Joe Black',
+          age: 28,
+          email: 'joe@example.com',
+          gender: 'Male',
+          address: '789 Broadway',
+          designation: 'Deckhand',
+        },
+        {
+          key: '4',
+          name: 'Jim Red',
+          age: 35,
+          email: 'jimred@example.com',
+          gender: 'Male',
+          address: '321 Elm St',
+          designation: 'Engineer',
+        },
+      ]);
+    }
+  }, []);
+
+  // Save data to local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem('resumeData', JSON.stringify(data));
+  }, [data]);
 
   const handleChange = (pagination, filters, sorter) => {
     console.log('Various parameters', pagination, filters, sorter);
@@ -35,6 +89,15 @@ const Resume = () => {
       sorter: (a, b) => a.name.localeCompare(b.name),
       sortOrder: sortedInfo.columnKey === 'name' ? sortedInfo.order : null,
       ellipsis: true,
+      render: (_, record) =>
+        record.editable ? (
+          <Input
+            value={record.name}
+            onChange={(e) => handleEditRow(record.key, { name: e.target.value })}
+          />
+        ) : (
+          record.name
+        ),
     },
     {
       title: 'Age',
@@ -43,21 +106,31 @@ const Resume = () => {
       sorter: (a, b) => a.age - b.age,
       sortOrder: sortedInfo.columnKey === 'age' ? sortedInfo.order : null,
       ellipsis: true,
+      render: (_, record) =>
+        record.editable ? (
+          <Input
+            type="number"
+            value={record.age}
+            onChange={(e) => handleEditRow(record.key, { age: parseInt(e.target.value, 10) || 0 })}
+          />
+        ) : (
+          record.age
+        ),
     },
     {
       title: 'Email',
       dataIndex: 'email',
       key: 'email',
-      sorter: (a, b) => a.email.localeCompare(b.email),
-      sortOrder: sortedInfo.columnKey === 'email' ? sortedInfo.order : null,
-      filters: [
-        { text: 'John', value: 'john@example.com' },
-        { text: 'Jim', value: 'jim@example.com' },
-        { text: 'Joe', value: 'joe@example.com' },
-      ],
-      filteredValue: filteredInfo.email || null,
-      onFilter: (value, record) => record.email.includes(value),
       ellipsis: true,
+      render: (_, record) =>
+        record.editable ? (
+          <Input
+            value={record.email}
+            onChange={(e) => handleEditRow(record.key, { email: e.target.value })}
+          />
+        ) : (
+          record.email
+        ),
     },
     {
       title: 'Gender',
@@ -72,22 +145,33 @@ const Resume = () => {
       filteredValue: filteredInfo.gender || null,
       onFilter: (value, record) => record.gender === value,
       ellipsis: true,
+      render: (_, record) =>
+        record.editable ? (
+          <Select
+            value={record.gender}
+            onChange={(value) => handleEditRow(record.key, { gender: value })}
+          >
+            <Select.Option value="Male">Male</Select.Option>
+            <Select.Option value="Female">Female</Select.Option>
+          </Select>
+        ) : (
+          record.gender
+        ),
     },
     {
       title: 'Address',
       dataIndex: 'address',
       key: 'address',
-      sorter: (a, b) => a.address.localeCompare(b.address),
-      sortOrder: sortedInfo.columnKey === 'address' ? sortedInfo.order : null,
-      filters: [
-        { text: 'Main St', value: '123 Main St' },
-        { text: 'Park Ave', value: '456 Park Ave' },
-        { text: 'Broadway', value: '789 Broadway' },
-        { text: 'Elm St', value: '321 Elm St' },
-      ],
-      filteredValue: filteredInfo.address || null,
-      onFilter: (value, record) => record.address.includes(value),
       ellipsis: true,
+      render: (_, record) =>
+        record.editable ? (
+          <Input
+            value={record.address}
+            onChange={(e) => handleEditRow(record.key, { address: e.target.value })}
+          />
+        ) : (
+          record.address
+        ),
     },
     {
       title: 'Designation',
@@ -96,55 +180,89 @@ const Resume = () => {
       sorter: (a, b) => a.designation.localeCompare(b.designation),
       sortOrder: sortedInfo.columnKey === 'designation' ? sortedInfo.order : null,
       filters: [
-        {text:'Captain', value:'Captain'},
-        {text:'First Mate', value:'First Mate'},
-        {text:'Deckhand', value:'Deckhand'},
-        {text:'Engineer',value:'Engineer'}
+        { text: 'Captain', value: 'Captain' },
+        { text: 'First Mate', value: 'First Mate' },
+        { text: 'Deckhand', value: 'Deckhand' },
+        { text: 'Engineer', value: 'Engineer' },
       ],
       filteredValue: filteredInfo.designation || null,
-      onFilter: (value,record) => record.designation.includes(value),
+      onFilter: (value, record) => record.designation.includes(value),
       ellipsis: true,
+      render: (_, record) =>
+        record.editable ? (
+          <Input
+            value={record.designation}
+            onChange={(e) => handleEditRow(record.key, { designation: e.target.value })}
+          />
+        ) : (
+          record.designation
+        ),
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      key: 'actions',
+      render: (_, record) => (
+        <Space>
+          {record.editable ? (
+            <>
+              <Button type="primary" onClick={() => handleSaveRow(record.key)}>
+                Save
+              </Button>
+              <Button onClick={() => handleCancelEdit(record.key)}>Cancel</Button>
+            </>
+          ) : (
+            <Button onClick={() => handleEditRow(record.key)}>Edit</Button>
+          )}
+          <Button onClick={() => handleDeleteRow(record.key)}>Delete</Button>
+        </Space>
+      ),
     },
   ];
 
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      email: 'john@example.com',
-      gender: 'Male',
-      address: '123 Main St',
-      designation: 'Captain',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      email: 'jim@example.com',
-      gender: 'Male',
-      address: '456 Park Ave',
-      designation: 'First Mate',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 28,
-      email: 'joe@example.com',
-      gender: 'Male',
-      address: '789 Broadway',
-      designation: 'Deckhand',
-    },
-    {
-      key: '4',
-      name: 'Jim Red',
-      age: 35,
-      email: 'jimred@example.com',
-      gender: 'Male',
-      address: '321 Elm St',
-      designation: 'Engineer',
-    },
-  ];
+  const handleAddRow = () => {
+    const newRow = {
+      key: Date.now().toString(),
+      name: 'New Person',
+      age: 0,
+      email: '', // Include the 'email' field for the new row
+      gender: '',
+      address: '',
+      designation: '',
+      editable: true,
+    };
+
+    setData((prevData) => [...prevData, newRow]);
+  };
+
+  const handleEditRow = (key, updatedRow) => {
+    const updatedData = data.map((row) =>
+      row.key === key ? { ...row, ...updatedRow } : row
+    );
+    setData(updatedData);
+  };
+
+  const handleSaveRow = (key) => {
+    const updatedData = data.map((row) => (row.key === key ? { ...row, editable: false } : row));
+    setData(updatedData);
+  };
+
+  const handleCancelEdit = (key) => {
+    // If the 'key' is 'Date.now()', it means this is a new row that hasn't been saved, so we remove it
+    let updatedData = data;
+    if (key === 'Date.now()') {
+      updatedData = data.filter((row) => row.key !== key);
+    } else {
+      // Otherwise, set the 'editable' flag to false for the existing row
+      updatedData = data.map((row) => (row.key === key ? { ...row, editable: false } : row));
+    }
+    setData(updatedData);
+  };
+
+  const handleDeleteRow = (key) => {
+    const updatedData = data.filter((row) => row.key !== key);
+    setData(updatedData);
+  };
 
   return (
     <>
@@ -156,6 +274,7 @@ const Resume = () => {
         <Button onClick={setAgeSort}>Sort age</Button>
         <Button onClick={clearFilters}>Clear filters</Button>
         <Button onClick={clearAll}>Clear filters and sorters</Button>
+        <Button onClick={handleAddRow}>Add Row</Button>
       </Space>
       <Table columns={columns} dataSource={data} onChange={handleChange} />
     </>
