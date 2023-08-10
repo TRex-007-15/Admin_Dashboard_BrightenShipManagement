b5313c0bc2d3860ca6097b571f2129410383616a
Afshankk <95470078+Afshankk@users.noreply.github.com>
2023-08-10 10:43:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,219 +1,113 @@
-import React, { useState } from "react";
-import {  getDownloadURL, uploadBytesResumable,ref} from "firebase/storage";
-import { storage, database } from "./firebase";
-import { Button, Form, Input, Select, message,Upload } from "antd";
-import { UploadOutlined } from "@ant-design/icons";
-import {ref as sRef,push, set } from "firebase/database";
+import React, { useState , useEffect  } from 'react';
+import { Layout, Menu,Button, Avatar } from 'antd';
+import { TeamOutlined, FileOutlined } from '@ant-design/icons';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation, Navigate } from 'react-router-dom';
+import logo from './logo.jpg';
+import Resume from './pages/Resume';
+import CrewPage from './pages/CrewPage';
+import Login from './Components/Login/Login';
 
+const { Header, Sider, Content } = Layout;
 
-const { Option } = Select;
-
-function App() {
-  const [progress, setProgress] = useState(0);
-  const [downloadURL, setDownloadURL] = useState(null);
-  const [formSubmitted, setFormSubmitted] = useState(false);
-
-    const uploadProps = {
-    beforeUpload: async (file) => {
-      console.log("Before upload:", file);
-      await uploadFiles(file);
-      return false; 
-    },
-  };
-  
-  
-  
-  const onFinish = async (values, file) => {
-    try {
-      console.log("Download URL:", downloadURL);
-      console.log("After upload:", file);
-
-     
-      
-  
-      const { name, email, indosNo, phoneNo, certificateType, rank } = values.user;
-      const formData = {
-        name,
-        email,
-        indosNo,
-        phoneNo,
-        certificateType,
-        rank,
-        resumeURL: downloadURL,
-      };
-  
-      const newEntryRef = push(sRef(database, "userFormData"));
-      await set(newEntryRef, formData);
-  
-      console.log("Form values:", formData);
-      console.log("New Entry Ref:", newEntryRef);
-   
-  
-      
-      message.success("Form submitted successfully.");
-      setFormSubmitted(true);
-    } catch (error) {
-      console.error("Error uploading file:", error);
-      
-    }
-  };
-  
-  
-  
-  
-
-  const uploadFiles = async (file) => {
-   
-    const storageRef = ref(storage, `files/${file.name}`);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-  
-    return new Promise((resolve, reject) => {
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const prog = Math.round(
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          );
-          setProgress(prog);
-        },
-        (error) => {
-          console.log("Error uploading file:", error);
-          reject(error);
-        },
-        async () => {
-          try {
-            const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-            console.log("Resolved Download URL:", downloadURL);  
-            resolve(downloadURL);
-            setDownloadURL(downloadURL);
-          } catch (error) {
-            console.error("Error getting download URL:", error);
-            reject(error);
-          }
-        }
-      );
-    });
-  
-  };
-  
-
+const Sidebar = () => {
+  const location = useLocation();
 
   return (
-    <div className="App">
-      {formSubmitted ? ( // Show success message if the form is submitted
-        <div style={{ textAlign: "center" }}>
-          <h1>Form submitted successfully!</h1>
-          <Button type="primary" onClick={() => window.location.reload()}>
-            Refresh Page
-          </Button>
-        </div>
-      ) : (
-        <Form
-          labelCol={{ span: 6 }}
-          wrapperCol={{ span: 14 }}
-          layout="horizontal"
-          onFinish={(values, file) => {
-            console.log(file);
-            onFinish(values, file);
-          }}
-          style={{ maxWidth: 600, margin: "auto", marginTop: 30, paddingTop: '20px' }} // Add margin between heading and form
-        >
-          <h1 style={{ textAlign: "center", marginBottom: 20 }}>Brighten Shipment's Application Form</h1>
-          <Form.Item
-            name={['user', 'name']}
-            label="Name"
-            rules={[
-              {
-                required: true,
-                message: "Please input your name!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name={['user', 'email']}
-            label="Email Address"
-            rules={[
-              {
-                required: true,
-                message: "Please input your email address!",
-                type: "email",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name={['user', 'indosNo']}
-            label="INDOS NO"
-            rules={[
-              {
-                required: true,
-                message: "Please input your INDOS number!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name={['user', 'phoneNo']}
-            label="Phone Number"
-            rules={[
-              {
-                required: true,
-                message: "Please input your phone number!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name={['user', 'certificateType']}
-            label="Certificate Type"
-            rules={[{ required: true, message: "Please select certificate type!" }]}
-          >
-            <Select>
-              <Option value="COC">COC</Option>
-              <Option value="Watchkeeping">Watchkeeping</Option>
-              <Option value="COP">COP</Option>
-              <Option value="COOK COP">COOK COP</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item
-            name={['user', 'rank']}
-            label="Rank"
-            rules={[{ required: true, message: "Please select rank!" }]}
-          >
-            <Select>
-              <Option value="Captain">Captain</Option>
-              <Option value="First Officer">First Officer</Option>
-              <Option value="Engineer">Engineer</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item label="Upload Resume" name="file">
-            <Upload {...uploadProps}>
-              <Button icon={<UploadOutlined />}>Click to Upload</Button>
-            </Upload>
-          </Form.Item>
-
-          <h5 style={{ textAlign: "center" }}>Uploaded {progress}%</h5>
-
-          <Form.Item style={{ textAlign: "center", marginTop: 20 }}>
-            <Button type="primary" htmlType="submit" disabled={!downloadURL}>
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      )}
-    </div>
+    <Menu theme="dark" mode="inline" selectedKeys={[location.pathname]}>
+      <Menu.Item key="/pages/resume" icon={<FileOutlined />}>
+        <Link to="/pages/resume">Resume</Link>
+      </Menu.Item>
+      <Menu.Item key="/pages/CrewPage" icon={<TeamOutlined/>}>
+        <Link to="/pages/CrewPage">Crew</Link>
+      </Menu.Item>
+    </Menu>
   );
-}
+};
 
-export default App;
+const Dashboard = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isInitialized, setIsInitialized] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Check if the user is already logged in (by checking the localStorage flag)
+    const isLoggedInLocal = localStorage.getItem('isLoggedIn');
+    setIsLoggedIn(isLoggedInLocal === 'true');
+    setIsInitialized(true); // Mark the login state as initialized
+    setIsLoading(false); // Set loading state to false once the login state is fetched
+  }, [isLoggedIn]); // Add isLoggedIn as a dependency here
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+    // Set the localStorage flag to indicate the user is logged in
+    localStorage.setItem('isLoggedIn', 'true');
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    // Clear the localStorage flag when the user logs out
+    localStorage.removeItem('isLoggedIn');
+  };
+
+  return (
+    <Router>
+      <Layout style={{ minHeight: '100vh', padding: '0px' }}>
+        {isInitialized ? (
+          // Only render when the login state is initialized
+          <>
+            {!isLoading ? (
+              // Render the content once loading is false
+              <>
+                {isLoggedIn ? (
+                  <>
+                    <Sider>
+                      <div className="logo" />
+                      <div style={{ padding: '16px', color: '#fff' }}>Dashboard</div>
+                      <Sidebar />
+                    </Sider>
+                    <Layout>
+                      <Header style={{ background: '#fff', padding: 0, display: 'flex', alignItems: 'center' }}>
+                        <div className="logo">
+                          <img
+                            src={logo}
+                            alt="Logo"
+                            style={{ height: '70px', marginRight: '16px', marginLeft: '30px', paddingTop: '30px' }}
+                          />
+                        </div>
+                        <h2 style={{ margin: 0, flex: 1, textAlign: 'center', paddingBottom: '12px' }}>
+                          Admin Dashboard
+                        </h2>
+                        {isLoggedIn && (
+                          <Button onClick={handleLogout} style={{ marginRight: '20px', marginLeft: 'auto' }}>
+                            Logout
+                          </Button>
+                        )}
+                      </Header>
+                      <Content style={{ margin: '16px' }}>
+                        <Routes>
+                          <Route path="/" element={<Navigate to="/pages/resume" />} />
+                          <Route path="/pages/resume" element={<Resume />} />
+                          <Route path="/pages/CrewPage" element={<CrewPage />} />
+                        </Routes>
+                      </Content>
+                    </Layout>
+                  </>
+                ) : (
+                  <Login onLoginSuccess={handleLoginSuccess} />
+                )}
+              </>
+            ) : (
+              // Show a loading placeholder if loading is true
+              <div>Loading...</div>
+            )}
+          </>
+        ) : (
+          // Show a loading placeholder while the login state is being initialized
+          <div>Loading...</div>
+        )}
+      </Layout>
+    </Router>
+  );
+};
+
+export default Dashboard;
